c0623acba37f7b935b6c548e9cbdf82ca002d832
yuchimiri <yuchimiri24@gmail.com>
2014-02-16 06:35:53 +0900

    Added mamenoki.

--- js/event.js
+++ js/event.js
@@ -2,3 +2,7 @@ function coinGet() {
   var $coinBlock = $('.coin-block');
   $('img', $coinBlock).attr('src', '../data/img/coinget.gif');
 }
+function grow() {
+  var $mamenokiBlock = $('.mamenoki-block');
+  $('img', $mamenokiBlock).attr('src', '../data/img/mamenoki.gif');
+}
