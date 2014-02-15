27cae6c9a0bfacf0c9cf357e2dcbc2e22ccb56c4
inuro <inuro@infofreako.com>
2014-02-16 06:46:16 +0900

    event fire method

--- js/event.js
+++ js/event.js
@@ -1,8 +1,8 @@
 function coinGet() {
   var $coinBlock = $('.coin-block');
-  $('img', $coinBlock).attr('src', '../data/img/coinget.gif');
+  $('img', $coinBlock).attr('src', 'data/img/coinget.gif');
 }
 function grow() {
   var $mamenokiBlock = $('.mamenoki-block');
-  $('img', $mamenokiBlock).attr('src', '../data/img/mamenoki.gif');
+  $('img', $mamenokiBlock).attr('src', 'data/img/mamenoki.gif');
 }
--- js/main.js
+++ js/main.js
@@ -8,10 +8,12 @@
     //contents canvas size
     var canvasSize = {"width": 1704, "height": 640};
     //viewport size
-    var viewSize = {"width": 568, "height": 320};
+    var viewSize = {"width": 640, "height": 480};
+//    var viewSize = {"width": 568, "height": 320};
     //var viewSize = {"width": 811.428571, "height": 457.142857};
     //(0,0)
-    var origin = {"x": 852, "y": 960};
+    var origin = {"x": 1570, "y": 960};
+  //  var origin = {"x": 852, "y": 960};
 
   //
   window.onReady(function onReady() {
@@ -23,10 +25,21 @@
     game.onload();
 
 
-    $('#infoframe').bind('click', function(){
-      $('#marioframe').show();
-      $('#infoframe').hide();
-    });
+    var changeMode = function(mode){
+      if(mode == 'mario'){
+        $('#marioframe').show();
+        $('#infoframe').hide();
+      }
+      else if(mode == 'resort'){
+        $('#resort').show();
+      }
+    } 
+
+
+
+
+
+
 
     //scroll method called from Objective-C
     functionTable["scroll"] = function(x,y){
@@ -36,11 +49,45 @@
 
       var debugmsg = "X:" + x + " / Y:" + y + " - Left:" + left + " / Top:" + top;
       console.log(debugmsg);
-      $('#debugarea').html(debugmsg);
+    //  $('#debugarea').html(debugmsg);
+    }
+
+    //start method
+    functionTable["start"] = function(){
+
+      changeMode('mario');
+    }
+
+    //event method
+    functionTable["fire"] = function(eventID){
+      //coin == 1
+      if(eventID == 1){
+        coinGet();
+      }
+      else if(eventID == 2){
+        grow();
+      }
     }
 
     //initial point
     entryPoint('scroll', 0, 0);
+
+
+
+    //test
+    $('#infoframe').bind('click', function(){
+      entryPoint('start');
+    //  changeMode('mario');
+    });
+
+    $('#coin').bind('click', function(){
+      entryPoint('fire', 1);
+    });
+    $('#mamenoki').bind('click', function(){
+      entryPoint('fire', 2);
+    });
+
+
   });
 
 
