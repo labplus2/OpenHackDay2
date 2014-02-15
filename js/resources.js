23b03a883bd1981e4f2356173759d1889c526f24
n1n9-jp <yuichi@n1n9.jp>
2014-02-15 21:34:32 +0900

    MapEditorからの出力に対応

---
+++ js/resources.js
@@ -0,0 +1,28 @@
+game.resources = [
+
+	/* 
+		Graphics.
+	 */
+    {name: "mapdata",  type:"image", src: "data/img/map/mapdata.png"},
+
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/*
+		Maps.
+	*/
+	{name: "area01", type: "tmx", src: "data/map/mario.tmx"},
+
+
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/", channel : 1},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/", channel : 2}
+	 */
+];
